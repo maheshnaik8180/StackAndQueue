3957ab4891081d9449988066a988fcfef215f42f
GENIUS <naikmahesh.naik@gmail.com>
2021-03-15 03:09:51 +0530

    Test Case for Check queue all element are stored

---
+++ src/main/java/com/bridgelabz/MyQueue.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+    public MyQueue() {
+        this.myLinkedList = new MyLinkedList();
+    }
+    public void queue(INode myNode) {
+        myLinkedList.add(myNode);
+    }
+    public void printQueue() {
+        myLinkedList.printMyNodes();
+    }
+    public INode peak() {
+        return myLinkedList.head;
+    }
+
+
+}
